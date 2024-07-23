e0918fbb5192160aa1ff47a7617cdfd7469d5b74
Peter Richmond <peter.richmond@outlook.com>
2024-07-24 00:06:24 +0100

    fix

--- build.bat
+++ build.bat
@@ -2,10 +2,7 @@ del target\output.hl
 del target\example.lua
 
 @REM Compile the code
-haxe -p src --main Generate --hl target\output.hl
-
-@REM Run the code
-hl .\target\output.hl
+haxe lovehaxe.hxml
 
 @REM Test the code
 haxe -p lib --main Main --lua test\main.lua -D lua-vanilla
